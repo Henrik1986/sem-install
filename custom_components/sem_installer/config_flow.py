514fc28f4875dc1c03459b388d1acb3957f9c55e
Henrik1986 <henrik.akerlund86@gmail.com>
2026-04-14 13:09:50 +0200

    Refactor config flow for SEM Installer

--- custom_components/sem_installer/config_flow.py
+++ custom_components/sem_installer/config_flow.py
@@ -1,33 +1,4 @@
-from homeassistant import config_entries
-import voluptuous as vol
-
-DOMAIN = "sem_installer"
-
-
-class SemInstallerFlow(config_entries.ConfigFlow, domain=DOMAIN):
-    """Config flow for SEM Installer."""
-
-    VERSION = 1
-
-    async def async_step_user(self, user_input=None):
-        """Start step with install button."""
-
-        if user_input is not None:
-
-            # Kör installationen
-            await self.hass.services.async_call(
-                DOMAIN,
-                "install"
-            )
-
-            # Skapa config entry (markera som installerad)
-            return self.async_create_entry(
-                title="SEM Installer",
-                data={"installed": True}
-            )
-
-        # Enkel form (ingen title/description här!)
-        return self.async_show_form(
-            step_id="user",
-            data_schema=vol.Schema({})
-        )
+return self.async_show_form(
+    step_id="user",
+    data_schema=vol.Schema({})
+)
